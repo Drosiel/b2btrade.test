ec2ec22f8f55749a3b2087db064bc2532bcccd03
Grimm <repisk@gmail.com>
2020-12-09 19:57:18 +0500

    first build

---
+++ build/precache-manifest.7f9225e0d95f56d576922b1c0b61ba99.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a5657f94196c5bd963cc947e9a0200e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5619be0391b5965cd9e",
+    "url": "/static/css/main.31f73cf2.chunk.css"
+  },
+  {
+    "revision": "b1034663452aabe55191",
+    "url": "/static/js/2.f03e3f15.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.f03e3f15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5619be0391b5965cd9e",
+    "url": "/static/js/main.22e959f0.chunk.js"
+  },
+  {
+    "revision": "7035e33fca0825184e7b",
+    "url": "/static/js/runtime-main.8a4488c1.js"
+  },
+  {
+    "revision": "0b15609c7dde5e151e1308550079448f",
+    "url": "/static/media/slider.0b15609c.jpg"
+  },
+  {
+    "revision": "0b7be79c4cc5ede6725bd39bea0f8731",
+    "url": "/static/media/style.0b7be79c.svg"
+  }
+]);
